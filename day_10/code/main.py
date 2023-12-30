a71db45d80853a520af03d69e63e30d7664de946
José Tomás Tocino <josetomas.tocino@gmail.com>
2023-12-30 17:08:04 +0100

    Add solution for day 10, part 2

--- day_10/code/main.py
+++ day_10/code/main.py
@@ -3,10 +3,25 @@ from __future__ import annotations
 import logging
 from collections import defaultdict
 from dataclasses import dataclass
-from enum import Enum
 from typing import Optional
 
 logger = logging.getLogger(__name__)
+
+
+def pmat(inp):
+    repl = {
+        '.': '.',
+        'L': '└',
+        'J': '┘',
+        'F': '┌',
+        '7': '┐',
+        '|': '│',
+        '-': '─',
+        '#': '#',
+        ' ': ' '
+    }
+
+    logger.info("\n" + '\n'.join(''.join(repl[x] for x in inpline) for inpline in inp))
 
 
 @dataclass
@@ -26,12 +41,17 @@ class Tile:
     right: Optional[Tile] = None
     bottom: Optional[Tile] = None
 
+    char: str = '.'
+    inside_hor: bool = False
+    inside_ver: bool = False
+
     def __repr__(self):
-        return f"Tile(left: {self.con_left}, top: {self.con_top}, right: {self.con_right}, bottom: {self.con_bottom} is_start={self.is_start}, x={self.x_coord}, y={self.y_coord})"
+        return f"Tile(is_start={self.is_start}, x={self.x_coord}, y={self.y_coord})"  # left: {self.con_left}, top: {self.con_top}, right: {self.con_right}, bottom: {self.con_bottom}
 
 
 def part_one(inp):
-    inp = inp.splitlines()
+    inp = [list(x) for x in inp.splitlines()]
+
     matrix = defaultdict(
         lambda: defaultdict(lambda: Tile(con_none=True, is_start=False, x_coord=None, y_coord=None)))
 
@@ -61,6 +81,7 @@ def part_one(inp):
                     t = Tile(con_none=True, is_start=True, x_coord=x, y_coord=y)
                     start_x, start_y = x, y
 
+            t.char = elem
             matrix[x][y] = t
 
     # Determine the shape type of starting tile
@@ -75,6 +96,26 @@ def part_one(inp):
     start_tile.con_right = neighbour_right.con_left
     start_tile.con_top = neighbour_top.con_bottom
     start_tile.con_bottom = neighbour_bottom.con_top
+
+    if start_tile.con_left:
+        if start_tile.con_right:
+            inp[start_y][start_x] = '-'
+
+        elif start_tile.con_top:
+            inp[start_y][start_x] = 'J'
+
+        elif start_tile.con_bottom:
+            inp[start_y][start_x] = '7'
+
+    elif start_tile.con_right:
+        if start_tile.con_top:
+            inp[start_y][start_x] = 'L'
+
+        elif start_tile.con_bottom:
+            inp[start_y][start_x] = 'F'
+
+    else:
+        inp[start_y][start_x] = '|'
 
     # Connect the tiles together
     for x in range(width):
@@ -98,8 +139,10 @@ def part_one(inp):
     prev = None
     steps = 0
 
+    loop_coords = set()
+
     while True:
-        logger.info(f"Current tile: {current}")
+        loop_coords.add((current.x_coord, current.y_coord))
 
         if current.left is not None and current.left != prev:
             prev = current
@@ -124,8 +167,65 @@ def part_one(inp):
 
     logger.info(f"Cycle found after {steps} steps")
 
-    return steps // 2
+    sol_part_one = steps // 2
 
+    pmat(inp)
 
-def part_two(inp):
-    pass
+    logger.info("Removing pipes that are not connected to the main loop")
+    for x in range(width):
+        for y in range(height):
+            if (x, y) not in loop_coords:
+                inp[y][x] = '.'
+                # matrix[x][y] ...
+
+    pmat(inp)
+
+    inside_nodes = 0
+
+    for x in range(width):
+        for y in range(height):
+            t = inp[y][x]
+
+            if t != '.':
+                continue
+
+            pipes_left = ''.join([inp[y][i] for i in range(0, x) if inp[y][i] in ['L', 'J', 'F', '7', '|']])
+            pipes_right = ''.join([inp[y][i] for i in range(x, width) if inp[y][i] in ['L', 'J', 'F', '7', '|']])
+            pipes_top = ''.join([inp[i][x] for i in range(0, y) if inp[i][x] in ['L', 'J', 'F', '7', '-']])
+            pipes_bottom = ''.join([inp[i][x] for i in range(y, height) if inp[i][x] in ['L', 'J', 'F', '7', '-']])
+
+            # In pipes_left and pipes_right a combination of LJ equals to a wall, same with F7, so remove those
+            pipes_left = pipes_left.replace('LJ', '').replace('F7', '')
+            pipes_right = pipes_right.replace('LJ', '').replace('F7', '')
+
+            # Also L7 equals to wall and FJ too
+            pipes_left = pipes_left.replace('L7', '|').replace('FJ', '|')
+            pipes_right = pipes_right.replace('L7', '|').replace('FJ', '|')
+
+            # In pipes_top and pìpes_bottom, a combination of FL equals to nothing, same with 7J
+            pipes_top = pipes_top.replace('FL', '').replace('7J', '')
+            pipes_bottom = pipes_bottom.replace('FL', '').replace('7J', '')
+
+            # Replace FJ and 7L with horizontal wall
+            pipes_top = pipes_top.replace('FJ', '-').replace('7L', '-')
+            pipes_bottom = pipes_bottom.replace('FJ', '-').replace('7L', '-')
+
+            logger.info(
+                f"Tile, '{elem}', at {x}, {y}      left: {pipes_left}, right: {pipes_right}, top: {pipes_top}, bottom: {pipes_bottom}")
+
+            # inplocal = deepcopy(inp)
+            # inplocal[y][x] = "#"
+            # pmat(inplocal)
+
+            plp = len(pipes_left) % 2
+            prp = len(pipes_right) % 2
+            ptp = len(pipes_top) % 2
+            pbp = len(pipes_bottom) % 2
+
+            if plp or prp or ptp or pbp:
+                logger.info(f"\tINSIDE NODE")
+                inside_nodes += 1
+
+    sol_part_two = inside_nodes
+
+    return sol_part_one, sol_part_two
--- day_10/test/test_main.py
+++ day_10/test/test_main.py
@@ -1,6 +1,6 @@
 import logging
 import os.path
-from ..code.main import part_one, part_two
+from ..code.main import part_one
 
 logger = logging.getLogger(__name__)
 local_path = os.path.abspath(os.path.dirname(__file__))
@@ -17,12 +17,47 @@ SJ.L7
 |F--J
 LJ..."""
 
+sample_input_3 = """...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."""
+
+sample_input_4 = """.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."""
+
+sample_input_5 = """FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"""
+
+
 def test_sample_input(caplog):
     caplog.set_level(logging.INFO)
 
-    assert part_one(sample_input) == 4
-    assert part_one(sample_input_2) == 8
-    # assert part_two(sample_input) == None
+    assert part_one(sample_input) == (4, 1)
+    assert part_one(sample_input_2) == (8, 1)
+    assert part_one(sample_input_3)[1] == 4
+    assert part_one(sample_input_4)[1] == 8
+    assert part_one(sample_input_5)[1] == 10
 
 
 def test_big_input(caplog):
@@ -30,5 +65,4 @@ def test_big_input(caplog):
     with open(os.path.join(local_path, "input"), "r") as f:
         content = f.read()
 
-        assert part_one(content) == 7102
-        # assert part_two(content) == None
+        assert part_one(content) == (7102, 363)
